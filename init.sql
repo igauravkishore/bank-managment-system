f8bab65b06d902579f298fc142a580b7bb42df9d
Gaurav Kishore <151769095+igauravkishore@users.noreply.github.com>
2025-08-22 00:43:08 +0530

    commit

---
+++ init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE account_db;
+CREATE DATABASE transaction_db;
+CREATE DATABASE user_db;
--- transaction-service/src/main/java/com/bankingsystem/transactionservice/model/Transaction.java
+++ transaction-service/src/main/java/com/bankingsystem/transactionservice/model/Transaction.java
@@ -1,4 +1,38 @@
 package com.bankingsystem.transactionservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String transactionId;
+    private String toAccountNumber;
+    private String fromAccountNumber;
+    private BigDecimal amount;
+    private LocalDate date;
+    @Enumerated(EnumType.STRING)
+    private TransactionType TransactionType;
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus TransactionStatus;
+
+    enum TransactionType{
+        TRANSFER, DEPOSIT, WITHDRAWAL
+    }
+
+    enum TransactionStatus{
+        PENDING, SUCCESS, FAILED
+    }
 }
